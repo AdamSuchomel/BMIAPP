31d0bd3dcaee9811b41553238d49552c9c922282
Adam Suchomel <sucho.adam@gmail.com>
2023-06-04 23:04:29 +0200

    code

--- Projekt/Charts/ViewModel.cs
+++ Projekt/Charts/ViewModel.cs
@@ -15,13 +15,58 @@ namespace Projekt.Charts
         {
             Data = new List<Models>()
         {
-            new Models { Time = "Leden ", BMI = 24 },
-            new Models { Time = "Únor", BMI = 26 },
-            new Models { Time = "Březen", BMI = 25 },
-            new Models { Time = "Duben", BMI = 23 },
-            new Models { Time = "Květen", BMI = 22 }
+            new Models { Time = "1990 ", BMI = 21.9 },
+            new Models { Time = "2000", BMI = 23.3 },
+            new Models { Time = "2010", BMI = 24.3 },
+            new Models { Time = "2020", BMI = 27.1 }
         };
-        }
+
+        //    string[] arrayclass;
+
+        //public List<Models> Data { get; set; }
+
+        //public ViewModel()
+        //{
+        //    Data = new List<Models>();
+
+
+        //    ReadFile();
+
+
+        //    //string[] arrayclass = { "75", "25", "30", "40" };
+
+        //    //Data = new List<Models>()
+        //    //    {
+        //    //             new Models { Time = "Leden ", BMI = Double.Parse(arrayclass[0]) },
+        //    //             new Models { Time = "Leden ", BMI = Double.Parse(arrayclass[1]) },
+        //    //             new Models { Time = "Leden ", BMI = Double.Parse(arrayclass[2]) },
+        //    //             new Models { Time = "Leden ", BMI = Double.Parse(arrayclass[3]) }
+        //    //        };
+        //}
+
+
+        //public async void ReadFile()
+        //{
+        //    string mainDir = FileSystem.Current.AppDataDirectory;
+        //    string fileName = "notesss.txt";
+        //    string filePath = System.IO.Path.Combine(mainDir, fileName);
+
+        //    try
+        //    {
+        //        using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync("BMI.txt");
+        //        using StreamReader reader = new StreamReader(fileStream);
+        //        string fileContent = await reader.ReadToEndAsync();
+        //        arrayclass = fileContent.Split(' ');
+        //        //lbl.Text = arrayclass[1] + arrayclass[2] + arrayclass[3];
+
+
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //}
+    }
+
     }
 }
 
